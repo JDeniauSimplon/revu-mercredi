59eb5c47164be4760733cb72395d1b3d40730d6c
JDeniau <jerome.deniau.simplon@gmail.com>
2023-03-08 13:55:06 +0100

    exercice

---
+++ demo.ts
@@ -0,0 +1,56 @@
+
+
+// // Exercice # 1 
+
+// Prendre la valeur du ticket client 
+// Verifier si la valeur est comprise entre 1 et 5
+// Si oui alors lui dire qu'il a gagné , sinon lui dire qu'il a perdu et l'inviter à recommencer
+
+// // Exercice # 2 
+
+// Prendre les valeurs des produits HT
+// additionner chaque valeurs
+// multiplier le résultat de l'addition par 1,2
+// donner le résultat final
+
+// // Exercice # 3
+
+// const organism : string ="Simplon"
+// const learners : number = 15
+// const promotionStarted : boolean = true
+// const learnerNames : any[] = ["Vincent", "Toufik", "Tom", "Samuel", "Orhan"]
+// const pedagogicalReferent : Record<string, string> = { name : "Tiphaine", work: "Simplon" }
+// const absentLearner: undefined = undefined
+
+// // Exercice #4
+
+// const country: string = "France";
+// function Exercice4 (){
+//     console.log(country);
+// }
+
+// Exercice4();
+
+// // Exercice #5
+
+// const firstName: string = "Jérôme";
+// const lastName: string = "DENIAU";
+
+// function Exercice5{
+//     console.log(firstName + " " + lastName);
+// }
+
+// Exercice5();
+
+// Exercice #6
+
+const adress: string = "201"
+const zipCode: number = 4000
+
+const adressFull: String = adress + " " + zipCode;
+
+function Exercice6{
+    console.log(adressFull)
+}
+
+Exercice6();
